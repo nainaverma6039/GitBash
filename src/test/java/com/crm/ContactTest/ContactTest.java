23e377428d87081074a92cddb16738d32a4d7a9d
nainaverma6039 <nainaverma1420@gmail.com>
2021-10-29 12:36:40 +0530

    first commit

---
+++ src/test/java/com/crm/ContactTest/ContactTest.java
@@ -0,0 +1,22 @@
+package com.crm.ContactTest;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	@Test
+	public void createContactTest()
+	{
+		System.out.println("execute createcontacttest");
+	}
+	@Test
+	public void editContactTest()
+	{
+		System.out.println("execute editcontacttest");
+	}
+	@Test
+	public void deleteContactTest()
+	{
+		System.out.println("execute deletecontacttest");
+	}
+	
+}
