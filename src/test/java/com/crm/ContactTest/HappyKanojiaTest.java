18a7ecbdb1521c682bfe2e72e70ef34d396006c2
nainaverma6039 <nainaverma1420@gmail.com>
2021-10-29 12:51:51 +0530

    third commit

---
+++ src/test/java/com/crm/ContactTest/HappyKanojiaTest.java
@@ -0,0 +1,23 @@
+package com.crm.ContactTest;
+
+import org.testng.annotations.Test;
+
+public class HappyKanojiaTest {
+	@Test
+	public void createContactTest()
+	{
+		System.out.println("execute createcontacttest");
+	}
+	@Test
+	public void editContactTest()
+	{
+		System.out.println("execute editcontacttest");
+	}
+	@Test
+	public void deleteContactTest()
+	{
+		System.out.println("execute deletecontacttest");
+	}
+	
+
+}
